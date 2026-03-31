13914099ee86ffed9cd8626e5b254786180d3524
Xander Warszawski <xander@xdoubleu.com>
2026-03-31 11:08:01 +0200

    Add footer showing version

--- internal/templates/main.go
+++ internal/templates/main.go
@@ -3,13 +3,23 @@ package templates
 import (
 	"embed"
 	"html/template"
+	"time"
+
+	"tools.xdoubleu.com/internal/config"
 )
 
 //go:embed html/shared/*.html
 var sharedFS embed.FS
 
-func LoadShared() *template.Template {
-	return template.Must(template.New("shared").ParseFS(
+func LoadShared(cfg config.Config) *template.Template {
+	return template.Must(template.New("shared").Funcs(template.FuncMap{
+		"release": func() string {
+			return cfg.Release
+		},
+		"copyrightYear": func() int {
+			return time.Now().Year()
+		},
+	}).ParseFS(
 		sharedFS,
 		"html/shared/*.html",
 	))
