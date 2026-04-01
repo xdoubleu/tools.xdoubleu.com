f9bf455f2f37974eccd103e67d55203a76726a82
Xander Warszawski <xander@xdoubleu.com>
2026-04-01 12:23:58 +0200

    Fix linter error

--- apps/goaltracker/internal/repositories/steam.go
+++ apps/goaltracker/internal/repositories/steam.go
@@ -66,7 +66,9 @@ func (repo *SteamRepository) UpsertGames(
 		completion_rate, contribution, has_achievements)
 		VALUES ($1, $2, $3, $4, $5, $6, $7)
 		ON CONFLICT (id, user_id)
-		DO UPDATE SET name = $3, is_delisted = $4, completion_rate = $5, contribution = $6, has_achievements = $7
+		DO UPDATE SET 
+		name = $3, is_delisted = $4, completion_rate = $5,
+		contribution = $6, has_achievements = $7
 	`
 
 	//nolint:exhaustruct //fields are optional
