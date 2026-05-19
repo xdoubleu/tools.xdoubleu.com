ddcc3178ac5531f723b158eca249dd918e9712c1
Xander Warszawski <xander@xdoubleu.com>
2026-05-20 01:27:49 +0200

    Attempt to fix run time env vars for web (#174)

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: 'tools.xdoubleu.com',
   description: 'Personal tools suite'
