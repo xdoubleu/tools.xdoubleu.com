504bcde6742ced61087721bbb4695d3381377b05
Xander Warszawski <xander@xdoubleu.com>
2026-07-25 14:34:56 +0200

    fix(global): robustness of migration 10 (#556)

--- api/cmd/api/migrations/00010_oauth_connections_config.sql
+++ api/cmd/api/migrations/00010_oauth_connections_config.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE global.oauth_connections ADD COLUMN config JSONB;
+ALTER TABLE global.oauth_connections ADD COLUMN IF NOT EXISTS config JSONB;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE global.oauth_connections DROP COLUMN config;
+ALTER TABLE global.oauth_connections DROP COLUMN IF EXISTS config;
 -- +goose StatementEnd
